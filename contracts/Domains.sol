5a4c5fcaa9f6292a6e069d2b684c7583fae07d09
AhmetSBulbul <ahmetsafabulbul@gmail.com>
2022-10-27 19:24:59 +0300

    storing domains

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -5,7 +5,21 @@ pragma solidity ^0.8.10;
 import "hardhat/console.sol";
 
 contract Domains {
+
+    mapping (string => address) public domains;
+
     constructor() {
         console.log("Deploying a Domains contract");
+    }
+
+    // Register a domain
+    function register(string calldata domain) public {
+        domains[domain] = msg.sender;
+        console.log("Registered domain %s for %s", domain, msg.sender);
+    }
+
+    // Get the owner of a domain
+    function getOwner(string calldata domain) public view returns (address) {
+        return domains[domain];
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,16 @@
 const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const domainContractFactory = await hre.ethers.getContractFactory("Domains");
     const domainContract = await domainContractFactory.deploy();
     await domainContract.deployed();
     console.log("Domain contract deployed to:", domainContract.address);
+    console.log("Contract deployed by:", owner.address);
+
+    const txn = await domainContract.register("test");
+    await txn.wait();
+
+    const domainOwner = await domainContract.getOwner("test");
+    console.log("Domain owner:", domainOwner);
 };
 
 const runMain = async () => {
